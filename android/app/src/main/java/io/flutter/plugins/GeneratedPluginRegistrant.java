efa16a5b6b3308898c0c509ae65f77c448b1d6e8
Tensor-Programming <abeltensor@tensor-programming.com>
2018-04-23 21:06:02 -0400

    initial commit tutorial part 3

---
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import io.intheloup.geolocation.GeolocationPlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    GeolocationPlugin.registerWith(registry.registrarFor("io.intheloup.geolocation.GeolocationPlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
+  }
+}
---
+++ lib/model/weather_repo.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:geolocation/geolocation.dart';
+
+import 'package:weather/json/response.dart';
+
+import 'package:weather/model/model.dart';
+import 'package:weather/const.dart';
+
+class WeatherRepo {
+  final http.Client client;
+
+  WeatherRepo({this.client});
+
+  int cnt = 50;
+
+  void addCities(int count) {
+    cnt = count;
+  }
+
+  Future<List<WeatherModel>> updateWeather(LocationResult result) async {
+    String url;
+    if (result != null) {
+      url =
+          'http://api.openweathermap.org/data/2.5/find?lat=${result.location.latitude}&lon=${result.location.longitude}&cnt=$cnt&appid=$API_KEY';
+    } else {
+      url =
+          'http://api.openweathermap.org/data/2.5/find?lat=43&lon=-79&cnt=10&appid=$API_KEY';
+    }
+
+    final response = await client.get(url);
+
+    List<WeatherModel> req = BaseResponse
+        .fromJson(json.decode(response.body))
+        .cities
+        .map((city) => WeatherModel.fromResponse(city))
+        .toList();
+
+    return req;
+  }
+
+  Future<LocationResult> updateLocation() async {
+    Future<LocationResult> result = Geolocation.lastKnownLocation();
+    return result;
+  }
+
+  Future<bool> getGps() async {
+    final GeolocationResult result = await Geolocation.isLocationOperational();
+    if (result.isSuccessful)
+      return true;
+    else
+      return false;
+  }
+}
